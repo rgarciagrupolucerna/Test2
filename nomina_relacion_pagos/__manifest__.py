2871217e4d3d30bbfe893420e8f90672d7273979
ReginaOtero <67808043+ReginaOtero@users.noreply.github.com>
2021-03-08 11:29:28 -0700

    nomina_relacion_pagos

---
+++ nomina_relacion_pagos/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Nomina Relacion Pagos",
+    "version": "14.0.0.1",
+    "category": "",
+    "summary": "",
+    "author": "",
+    "website": "",
+    "license": "AGPL-3",
+    "depends": ['nomina_cfdi_extras_ee'],
+    "data": [
+        "report/relacion_de_pagos_xls_view.xml",
+        ],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ nomina_relacion_pagos/report/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from . import relacion_de_pagos_xls
